76c76ed657bcb6056709a64f37dc10888c9ad003
HUH SEONGWOOK <chronic794@naver.com>
2019-03-23 17:33:57 +0900

    Initial commit

---
+++ src/main/java/com/calia/CaliaApplication.java
@@ -0,0 +1,13 @@
+package com.calia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaliaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaliaApplication.class, args);
+    }
+
+}
